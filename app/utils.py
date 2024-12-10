fce3b04241d7ac2bedd1dc252a4ec48c7ce55d22
lostdd <none@none.none>
2024-12-10 12:33:02 +0300

    Revert "Intitial commit"
    This reverts commit 2fb4c6ecffddde21c26621b99e8647acdad5f9c5.

--- app/utils.py
+++
@@ -1,6 +0,0 @@
-import random
-import string
-
-def generate_token(length: int = 64) -> str:
-    letters = string.ascii_letters + string.digits
-    return ''.join(random.choice(letters) for _ in range(length))
